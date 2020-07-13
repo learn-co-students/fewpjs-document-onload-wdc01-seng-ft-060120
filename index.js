2318b44d3812acc5381086730cfafadb57763a55
devdave0 <dbchung24@gmail.com>
2020-07-13 13:59:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateText()
+  });
+
+   
+
+
+function updateText() {
+    document.getElementById('text').innerHTML = "This is really cool!";
+}
